0ac6dd519328710845d162c3d00818030ec7063b
Emmanuella Owusu <159191512+ABoahenmaa@users.noreply.github.com>
2026-02-16 20:21:25 +0000

    final

--- assignment/trials.py
+++ assignment/trials.py
@@ -9,31 +9,32 @@
 import random
 
 player = input("What's your name?")
+
 play_again = True
 
 while play_again:
     number = random.randint(1, 10)
     tries = 3
 
-print(f"Welcome {player} to the Guess the Number Game!"
-      f"You have only {tries} tries to guess the number between 1 and 10."
-      f"Use the hints wisely to help you win the game!!")
+    print(f"Welcome {player} to the Guess the Number Game!"
+          f"You have only {tries} tries to guess the number between 1 and 10."
+          f"Use the hints wisely to help you win the game!!")
 
-for attempt in range(1, tries + 1):
-    guess = int(input(f"{player}, guess a number between 1 and 10:"))
+    for attempt in range(1, tries + 1):
+        guess = int(input(f"{player}, guess a number between 1 and 10:"))
 
-    if guess == number:
-        print(f"{player} you won!!")
-        break
-    elif guess < number:
-        print(f"{player}, Enter a higher number")
-    elif guess > number:
-        print(f"{player}, Enter a lesser number")
+        if guess == number:
+            print(f"{player} you won!!")
+            break
+        elif guess < number:
+            print(f"{player}, Enter a higher number")
+        elif guess > number:
+            print(f"{player}, Enter a lesser number")
 
-else:
-    print(f"Game over {player}! The number was {number}")
+    else:
+        print(f"Game over {player}! The number was {number}")
 
-again = input("Do you want to play again? (yes/no): ").strip().lower()
-if again != "yes":
-    play_again = False
-    print(f"Thanks for playing, {player}! byeee!")
+    again = input("Do you want to play again? (yes/no): ").strip().lower()
+    if again != "yes":
+        play_again = False
+        print(f"Thanks for playing, {player}! byeee!")
